050b89c9460ad2e894ed52501558f212967e738c
jhon velasco <jhonvelasco@cotecnova.edu.co>
2021-05-30 22:34:40 -0500

    notas

--- App.js
+++ App.js
@@ -1,21 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Modal,
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+} from "react-native";
+import Item from "./Item";
+import Button from "./Button";
+import Input from "./Input";
+
+export default class App extends React.Component {
+  state = {
+    data: [],
+    isVisible: false,
+    text: ""
+  };
+  handlePress = () => {
+    this.setState({ isVisible: true });
+  };
+
+  handleChange = text => this.setState({ text });
+
+  handleSave = () => {
+    const { text, data } = this.state;
+    const datos = [{ key: Math.random().toString(), title: text }].concat(data);
+    this.setState({
+      data: datos,
+      isVisible: false,
+      text: ""
+    });
+  };
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <View style={[styles.container, styles.gray]}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Mis Notas</Text>
+        </View>
+        <View style={styles.view}>
+          <Button title="Nueva Nota" onPress={this.handlePress} />
+        </View>
+        <FlatList data={data} renderItem={Item} />
+        <Modal animationType="slide" visible={this.state.isVisible}>
+          <View style={[styles.container, styles.center]}>
+            <Text style={styles.modalTitle}>Ingrese recordatorio</Text>
+            <Input
+              onChangeText={this.handleChange}
+              placeholder="Recordatorio"
+              value={this.state.text}
+            />
+            <Button title="Guardar" onPress={this.handleSave} />
+          </View>
+        </Modal>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  center: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  header: {
+    height: 100,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd"
+  },
+  title: {
+    textAlign: "center",
+    marginTop: 50,
+    fontSize: 28
+  },
+  view: {
+    height: 50,
+    margin: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  gray: {
+    backgroundColor: "#eee"
+  },
+  modalTitle: {
+    fontSize: 28
+  }
 });
---
+++ Input.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { TextInput, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  view: {
+    height: 50,
+    margin: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 15
+  }
+});
+export default ({ placeholder, onChangeText, value }) => (
+  <View style={styles.view}>
+    <TextInput
+      onChangeText={onChangeText}
+      style={styles.input}
+      placeholder={placeholder}
+      value={value}
+    />
+  </View>
+);
+
